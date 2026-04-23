5f4d07bf200e9083e4c97b1137ca23691d0e2658
stephwlsn-design <stef.soul.wlsn@gmail.com>
2026-04-23 13:05:09 +0530

    Update the OTTO script

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,9 @@ export default function Document() {
         <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
 
+        {/* <script async src="http://localhost:8000/pixel.js?id=OTTO-PX-5DAE1720"></script> */}
+        <script async src="https://ottoplatform-410578063330.us-central1.run.app/pixel.js?id=OTTO-PX-CAD5ED57"></script>
+
         {/* Google Analytics (gtag.js) */}
         <script
           async
